5f0d43fc15db1fb3f4fd667eb9c503dd3ddb6893
Der Archivar <d.archivar@gmail.com>
2018-02-02 00:10:54 +0100

    Initial commit of chocolatey package sources of scenebuilder9 package.

---
+++ scenebuilder9/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'SceneBuilder-9.0.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file          = $fileLocation
+
+  softwareName  = 'SceneBuilder*'
+
+  checksum      = '0B51FEFB6F744C6FB12BF0BC22D2496D1944D2E45476098BDC8C416B64785A7C'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
