2f3de912e369d18d3fe79f268570c6116bf8b128
Der Archivar <d.archivar@gmail.com>
2018-03-02 10:39:50 +0100

    Upgraded Nexus Mod Manager (nmm) to version 0.64.2.

--- nmm/tools/chocolateyinstall.ps1
+++ nmm/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'Nexus.Mod.Manager-0.64.1.exe'
+$fileLocation = Join-Path $toolsDir 'Nexus.Mod.Manager-0.64.2.exe'
 
 $packageArgs = @{
 	packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
 
 	softwareName  = 'Nexus Mod Manager'
 
-	checksum      = '2B128CBCC8CD5C28C65F5BB6DBA2AD72CFC5E79D8C8C6081012AA310AB92A0D8'
+	checksum      = '09FD6AA3F435B8CADFB8D06128B1B0F57B5E2D89D06987B1A73E1E6011806153'
 	checksumType  = 'sha256'
 
 	silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
