a39d0419e7746f5373f1160bea54be6f2696aa80
Der Archivar <d.archivar@gmail.com>
2018-03-02 14:53:05 +0100

    Added initial package for application 'LOOT: The Load Order Optimisation Tool' and the appropriate icon. Icon URL still missing from nuspec file, because the link will be generated after this commit and push.

---
+++ loot/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'LOOT.Installer.exe'
+
+$packageArgs = @{
+	packageName   = $env:ChocolateyPackageName
+	fileType      = 'exe'
+	file          = $fileLocation
+
+	softwareName  = 'LOOT'
+
+	checksum      = '98B04A3118F12306F35D4EF8948F5F8A8D62B80702326D0FA36391E34DB9AC16'
+	checksumType  = 'sha256'
+
+	silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+	validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
