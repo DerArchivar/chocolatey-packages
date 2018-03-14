407b2c750a9c44be0cd63211b4b5c1bb9db88746
Der Archivar <d.archivar@gmail.com>
2018-03-14 14:49:24 +0100

    Added google-hangouts-chat in version 18.2.252 to chocolatey-packages, as well as adding the appropriate logo.

---
+++ google-hangouts-chat/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$url = 'https://dl.google.com/chat/18.2.252/InstallHangoutsChat.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url			= $url
+
+  softwareName  = 'Hangouts Chat'
+
+  checksum      = 'E8960878A177CE0738CAF2DCEAE716070C1148792689C220699BCED06763720C'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/quiet /norestart'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
